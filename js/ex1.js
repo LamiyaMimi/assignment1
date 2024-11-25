eef2b65ffec48d59bbf5cbcdff41f963bd072225
LamiyaMimi <lamiyamimi625@gmail.com>
2024-11-25 12:46:57 +0600

    update

--- js/ex1.js
+++ js/ex1.js
@@ -1,65 +1,90 @@
 
 
-console.log("ggjgjg");
+var firstname=" Hi,my name is joya das."
+ var secondname="i'm 21 year old."
 
-document.getElementById("student_message").innerHTML = "my name is Lamiya Mimi, I'm 22 years old and I'm learning Javascript";
+document.getElementById("student_message").innerHTML = "Hi, my name is joya das." + ""+ "i'm 21 year old.";
 
-document.getElementById("result").innerHTML = "14.429";
 
+//2//
+
+var number1=101;
+var number2=7;
+
+document.getElementById("result").innerHTML= number1/number2
+
+//3//
 
 var phone1 = "988866552";
 var phone2 = "99087612366";
-var phone3 = "876543123";
+var phone3 = 876543123;
 
-if(phone1.length ==9){
-    console.log(phone1+ "is valid")
-}else{
+if(phone1.length ==9) {
+    console.log(phone1+"is valid");
+} 
+else (phone1.length == 9) 
+{
     console.log(phone1+ "is not valid")
 }
 
-if(phone2.length ==9){
-    console.log(phone1+ "is valid")
-}else{
-    console.log(phone3+ "is not valid")
+
+if (phone2.length == 9) {
+    console.log(phone2 + "is valid");
+}
+else (phone2.length == 9) 
+{
+    console.log(phone2 + "is not valid")
 }
 
-if(phone3.length ==9){
-    console.log(phone1+ "is valid")
-}else{
-    console.log(phone3+ "is not valid")
+
+if (phone3.length == 9) {
+    console.log(phone3 + "is valid");
+}
+else (phone3.length == 9) 
+{
+    console.log(phone3 + "is not valid")
 }
 
-var power = Math.pow(32,6);  
-console.log(power)
+//5//
+ var power = Math.pow(32,6)
+ console.log(power)
 
 
-var getAddress = document.getElementById("url_1").innerText;
-var modifiedAddress = getAddress + " " +"https:// here";
+ //6//
+var quantity = "25";
 
-document.getElementById("url_2").innerText = modifiedAddress;
+console.log(quantity += quantity);
+
+var number = 6;
+console.log((7 + 5) / number + 2);
+
+var pressure;
+console.log(pressure);
+console.log(typeof pressure); 
+
+var temperature = null;
+console.log(temperature); 
+console.log(typeof temperature);
+
+//7//
+
+// 1
+var getAddressFromWebsite = document.getElementById("url_1").innerText;
+console.log(getAddressFromWebsite);
+
+var modifyAddress = "https://" + getAddressFromWebsite;
+console.log(modifyAddress);
 
 
+document.getElementById("url_2").innerText = modifyAddress;
 
 
-                
+// 2
 
 var getAddress = document.getElementById("url_3").innerText;
-var replace = getAddress + "https:// here";
+console.log(getAddress);
 
-document.getElementById("url_4").innerText = replace;
+var modifyAddress = (getAddress.replace("https://", ""));
+console.log(modifyAddress);
 
-
-
-
-
-var quantity = "25";
-var number = 6;
-var pressure;
-var temperature = null;
-
-console.log(quantity += quantity); 
-console.log( (7+5) / number + 2 ); 
-console.log(pressure); 
-console.log(temperature); 
-console.log(typeof pressure); 
-console.log(typeof temperature);
+document.getElementById("url_4").innerText = modifyAddress;
